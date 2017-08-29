972579f8525fa8bfa9dcb27b68f47646252774d7
dmk2014 <6460241+dmk2014@users.noreply.github.com>
2017-08-29 22:48:06 +0100

    Add interface DayOne. Refactor dayone.Import to use interface. Remove knowledge of Momento implemenation and export format from dayone package.

--- dayone/importer.go
+++ dayone/importer.go
@@ -4,70 +4,44 @@ import (
 	"fmt"
 	"os/exec"
 	"path"
-	"strings"
-
-	"github.com/dmk2014/momento2dayone/momento"
 )
 
-var months = make(map[string]string)
-
-func init() {
-	months["January"] = "01"
-	months["Feburary"] = "02"
-	months["March"] = "03"
-	months["April"] = "04"
-	months["May"] = "05"
-	months["June"] = "06"
-	months["July"] = "07"
-	months["August"] = "08"
-	months["September"] = "09"
-	months["October"] = "10"
-	months["November"] = "11"
-	months["December"] = "12"
+// DayOne is an interface that defines the contract for an entry in
+// a DayOne journal.
+type DayOne interface {
+	Text() string
+	ISODate() string
+	Tags() []string
+	Media(suffix string) []string
 }
 
-// Import iterates over the provided moments and utilizes the dayone2
+// Import iterates over the provided entries and utilizes the dayone2
 // CLI to add them to DayOne.
-func Import(moments []momento.Moment) {
-	for i := 0; i < 10; i++ {
-		m := moments[i]
+func Import(entries []DayOne) {
+	for i, m := range entries {
+		if i > 10 {
+			break
+		}
 
 		args := make([]string, 0, 20)
 
-		// Text
 		args = append(args, "new")
-		args = append(args, m.Text)
+		args = append(args, m.Text())
 
-		// Date (yyyy-mm-dd [hh:mm])
-		dateParts := strings.Split(m.Date, " ")
-		day := dateParts[0]
-		month := months[dateParts[1]]
-		year := dateParts[2]
-		date := fmt.Sprintf("%s-%s-%s %s", year, month, day, m.Time)
 		args = append(args, "-d")
-		args = append(args, date)
+		args = append(args, m.ISODate())
 
-		// Tags
-		if len(m.Tags) > 0 || len(m.People) > 0 || len(m.Places) > 0 {
+		if tags := m.Tags(); len(tags) > 0 {
 			args = append(args, "-t")
-			args = append(args, m.Tags...)
-			args = append(args, m.People...)
-			args = append(args, m.Places...)
+			args = append(args, tags...)
 		}
 
 		// Images (DayOne2 does not support video at present)
-		if len(m.Media) > 0 {
-			argsSet := false
-			for _, media := range m.Media {
-				absPath := path.Join("/Users/darren/Desktop/Momento Export 2017-08-13 16_27_04/Attachments", media)
-				if path.Ext(absPath) != ".jpg" {
-					// TODO: Log media not added
-					continue
-				}
-				if !argsSet {
-					args = append(args, "-p")
-					argsSet = true
-				}
+		if media := m.Media(".jpg"); len(media) > 0 {
+			args = append(args, "-p")
+			for _, image := range media {
+				// TODO: Should Moment expose Path or just filename?
+				absPath := path.Join("/Users/darren/Desktop/Momento Export 2017-08-13 16_27_04/Attachments", image)
 				args = append(args, absPath)
 			}
 		}
