a189a6f67ad2a97e6a64ebb436738f6b06e2f5ae
dmk2014 <6460241+dmk2014@users.noreply.github.com>
2017-09-11 22:42:33 +0100

    Refactoring.

--- main.go
+++ main.go
@@ -17,49 +17,14 @@ func main() {
 	if err := initializeLog(); err != nil {
 		log.Fatal("Logger could not be initialized.")
 	}
-
 	log.Print("Momento2DayOne Session Beginning.")
 
-	if runtime.GOOS != "darwin" {
-		log.Fatal("macOS Required (this is the only platform on which the Day One CLI is available).")
-	}
-	if err := exec.Command("dayone2").Run(); err != nil {
-		log.Fatalf("Day One CLI not found. See %q for install instructions.",
-			"http://help.dayoneapp.com/day-one-2-0/command-line-interface-cli")
-	}
+	validateRuntime()
 
-	// Parse Momento Export
-	basePath := "/Users/darren/Desktop/Momento Export 2017-08-13 16_27_04"
-	exportPath := path.Join(basePath, "Export.txt")
-	mediaPath := path.Join(basePath, "Attachments")
+	moments := parseMomentoExport("/Users/darren/Desktop/Momento Export 2017-08-13 16_27_04")
+	importToDayOne(moments)
 
-	file, err := os.Open(exportPath)
-	if err != nil {
-		log.Fatal("Momento export could not be opened. Verify path and try again.")
-	}
-	defer file.Close()
-
-	moments, err := momento.Parse(file, mediaPath)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	expectedMoments := 6134
-	if expectedMoments != len(moments) {
-		// TODO
-	}
-
-	// Import to DayOne
-	// https://npf.io/2014/05/intro-to-go-interfaces/
-	// https://stackoverflow.com/questions/12994679/golang-slice-of-struct-slice-of-interface-it-implements
-	// TODO: research pointer receivers, conversion and duplication issue when using &m
-	entries := make([]dayone.DayOne, len(moments))
-	for i, m := range moments {
-		entries[i] = dayone.DayOne(m)
-	}
-
-	dayone.Import(entries)
-
+	log.Print("Momento2DayOne Session Exiting Successfully.")
 	os.Exit(0)
 }
 
@@ -72,4 +37,49 @@ func initializeLog() (err error) {
 	log.SetOutput(file)
 
 	return
+}
+
+func validateRuntime() {
+	if runtime.GOOS != "darwin" {
+		log.Fatal("macOS Required (this is the only platform on which the Day One CLI is available).")
+	}
+	if err := exec.Command("dayone2").Run(); err != nil {
+		log.Fatalf("Day One CLI not found. See %q for install instructions.",
+			"http://help.dayoneapp.com/day-one-2-0/command-line-interface-cli")
+	}
+}
+
+func parseMomentoExport(basePath string) []momento.Moment {
+	exportPath := path.Join(basePath, "Export.txt")
+	mediaPath := path.Join(basePath, "Attachments") // TODO: Ensure path exists.
+
+	file, err := os.Open(exportPath)
+	if err != nil {
+		log.Fatal("Momento export could not be opened. Verify path and try again.")
+	}
+	defer file.Close()
+
+	moments, err := momento.Parse(file, mediaPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	expected := 6134
+	if expected != len(moments) {
+		log.Fatalf("Moment count mismatch. Expected: %d. Actual: %d.", expected, len(moments))
+	}
+
+	return moments
+}
+
+func importToDayOne(moments []momento.Moment) {
+	// https://npf.io/2014/05/intro-to-go-interfaces/
+	// https://stackoverflow.com/questions/12994679/golang-slice-of-struct-slice-of-interface-it-implements
+	// TODO: research pointer receivers, conversion and duplication issue when using &m
+	entries := make([]dayone.DayOne, len(moments))
+	for i, m := range moments {
+		entries[i] = dayone.DayOne(m)
+	}
+
+	dayone.Import(entries)
 }
