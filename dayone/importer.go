9048df86681be1d253f80d616d88aa52d49a4730
dmk2014 <6460241+dmk2014@users.noreply.github.com>
2017-10-07 15:27:18 +0100

    Extract args creation to function.

--- dayone/importer.go
+++ dayone/importer.go
@@ -24,39 +24,20 @@ func Import(entries []DayOne) {
 	log.Printf("DayOne Import Starting. %d Entries.", len(entries))
 	start := time.Now()
 
-	for i, m := range entries {
+	for i, entry := range entries {
+		if i >= 10 {
+			break
+		}
+
 		fmt.Printf("\rDayOne Import Running - Entry %d of %d.", i+1, len(entries))
 
-		args := make([]string, 0, 20)
-
-		args = append(args, "new")
-		args = append(args, m.Text())
-
-		args = append(args, "--isoDate")
-		args = append(args, m.ISODate())
-
-		args = append(args, "-z")
-		args = append(args, "UTC")
-
-		if tags := m.Tags(); len(tags) > 0 {
-			args = append(args, "-t")
-			args = append(args, tags...)
-		}
-
-		// Images (DayOne2 does not support video at present)
-		if media := m.Media(".jpg"); len(media) > 0 {
-			args = append(args, "-p")
-			args = append(args, media...)
-		}
-
-		// Ignore Standard In (default behaviour if no text argument provided)
-		args = append(args, "--no-stdin")
+		args := getArgs(entry)
 
 		cmd := exec.Command("dayone2", args...)
 		output, err := cmd.CombinedOutput()
 		if err != nil {
 			errors++
-			log.Printf("Entry with date %q could not be imported.", m.ISODate())
+			log.Printf("Entry with date %q could not be imported.", entry.ISODate())
 			log.Printf(string(output))
 			continue
 		}
@@ -72,4 +53,33 @@ func Import(entries []DayOne) {
 
 	duration := time.Since(start)
 	log.Printf("DayOne Import Complete. Imported: %d, Errors: %d. Imported in %q.", imported, errors, duration.String())
+}
+
+func getArgs(entry DayOne) []string {
+	args := make([]string, 0, 20)
+
+	args = append(args, "new")
+	args = append(args, entry.Text())
+
+	args = append(args, "--isoDate")
+	args = append(args, entry.ISODate())
+
+	args = append(args, "--time-zone")
+	args = append(args, "UTC")
+
+	if tags := entry.Tags(); len(tags) > 0 {
+		args = append(args, "--tags")
+		args = append(args, tags...)
+	}
+
+	// Images (DayOne2 does not support video at present)
+	if media := entry.Media(".jpg"); len(media) > 0 {
+		args = append(args, "--photos")
+		args = append(args, media...)
+	}
+
+	// Ignore Standard In (default behaviour if no text argument provided)
+	args = append(args, "--no-stdin")
+
+	return args
 }
